0f7fb81657d93485a70eb96691ccb64f118fc2b1
Sebastien Jodogne <s.jodogne@gmail.com>
2012-07-24 10:03:17 +0200

    fixes for mingw

--- Resources/CMake/BoostConfiguration.cmake
+++ Resources/CMake/BoostConfiguration.cmake
@@ -26,8 +26,9 @@ if (BOOST_STATIC)
   SET(BOOST_SOURCES_DIR ${CMAKE_BINARY_DIR}/${BOOST_NAME})
   DownloadPackage("http://switch.dl.sourceforge.net/project/boost/boost/1.49.0/${BOOST_NAME}.tar.gz" "${BOOST_SOURCES_DIR}" "${BOOST_PRELOADED}" "${BOOST_NAME}/boost ${BOOST_NAME}/libs/thread/src ${BOOST_NAME}/libs/system/src ${BOOST_NAME}/libs/filesystem/v3/src ${BOOST_NAME}/libs/locale/src")
 
+  set(BOOST_SOURCES)
   if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-    list(APPEND THIRD_PARTY_SOURCES 
+    list(APPEND BOOST_SOURCES
       ${BOOST_SOURCES_DIR}/libs/thread/src/pthread/once.cpp
       ${BOOST_SOURCES_DIR}/libs/thread/src/pthread/thread.cpp
       )
@@ -35,7 +36,7 @@ if (BOOST_STATIC)
       -DBOOST_LOCALE_WITH_ICONV=1
       )
   elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
-    list(APPEND THIRD_PARTY_SOURCES 
+    list(APPEND BOOST_SOURCES
       ${BOOST_SOURCES_DIR}/libs/thread/src/win32/tss_dll.cpp
       ${BOOST_SOURCES_DIR}/libs/thread/src/win32/thread.cpp
       ${BOOST_SOURCES_DIR}/libs/thread/src/win32/tss_pe.cpp
@@ -48,7 +49,7 @@ if (BOOST_STATIC)
     message(FATAL_ERROR "Support your platform here")
   endif()
 
-  list(APPEND THIRD_PARTY_SOURCES 
+  list(APPEND BOOST_SOURCES
     ${BOOST_SOURCES_DIR}/libs/system/src/error_code.cpp
     ${BOOST_SOURCES_DIR}/libs/filesystem/v3/src/path.cpp
     ${BOOST_SOURCES_DIR}/libs/filesystem/v3/src/path_traits.cpp
@@ -56,6 +57,8 @@ if (BOOST_STATIC)
     ${BOOST_SOURCES_DIR}/libs/filesystem/v3/src/codecvt_error_category.cpp
     ${BOOST_SOURCES_DIR}/libs/locale/src/encoding/codepage.cpp
     )
+
+  list(APPEND THIRD_PARTY_SOURCES ${BOOST_SOURCES})
 
   add_definitions(
     # Static build of Boost
@@ -68,6 +71,10 @@ if (BOOST_STATIC)
     -DBOOST_SYSTEM_NO_LIB
     -DBOOST_LOCALE_NO_LIB
     )
+
+  if (${CMAKE_COMPILER_IS_GNUCXX})
+    add_definitions(-isystem ${BOOST_SOURCES_DIR})
+  endif()
 
   include_directories(
     ${BOOST_SOURCES_DIR}
