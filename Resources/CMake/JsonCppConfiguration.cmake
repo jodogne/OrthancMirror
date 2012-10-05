42a33aa269ff81a2846d7c6198b35005e8959b29
Sebastien Jodogne <s.jodogne@gmail.com>
2012-10-05 15:23:20 +0200

    dynamic jsoncpp in debian unstable

--- Resources/CMake/JsonCppConfiguration.cmake
+++ Resources/CMake/JsonCppConfiguration.cmake
@@ -1,14 +1,25 @@
-SET(JSONCPP_SOURCES_DIR ${CMAKE_BINARY_DIR}/jsoncpp-src-0.5.0)
-DownloadPackage("http://downloads.sourceforge.net/project/jsoncpp/jsoncpp/0.5.0/jsoncpp-src-0.5.0.tar.gz" "${JSONCPP_SOURCES_DIR}" "" "")
+if (USE_DYNAMIC_JSONCPP)
+  CHECK_INCLUDE_FILE_CXX(jsoncpp/json/reader.h HAVE_JSONCPP_H)
+  if (NOT HAVE_JSONCPP_H)
+    message(FATAL_ERROR "Please install the libjsoncpp-dev package")
+  endif()
 
-list(APPEND THIRD_PARTY_SOURCES
-  ${JSONCPP_SOURCES_DIR}/src/lib_json/json_reader.cpp
-  ${JSONCPP_SOURCES_DIR}/src/lib_json/json_value.cpp
-  ${JSONCPP_SOURCES_DIR}/src/lib_json/json_writer.cpp
-  )
+  link_libraries(jsoncpp)
 
-include_directories(
-  ${JSONCPP_SOURCES_DIR}/include
-  )
+else()
+  SET(JSONCPP_SOURCES_DIR ${CMAKE_BINARY_DIR}/jsoncpp-src-0.5.0)
+  DownloadPackage("http://downloads.sourceforge.net/project/jsoncpp/jsoncpp/0.5.0/jsoncpp-src-0.5.0.tar.gz" "${JSONCPP_SOURCES_DIR}" "" "")
 
-source_group(ThirdParty\\JsonCpp REGULAR_EXPRESSION ${JSONCPP_SOURCES_DIR}/.*)
+  list(APPEND THIRD_PARTY_SOURCES
+    ${JSONCPP_SOURCES_DIR}/src/lib_json/json_reader.cpp
+    ${JSONCPP_SOURCES_DIR}/src/lib_json/json_value.cpp
+    ${JSONCPP_SOURCES_DIR}/src/lib_json/json_writer.cpp
+    )
+
+  include_directories(
+    ${JSONCPP_SOURCES_DIR}/include
+    )
+
+  source_group(ThirdParty\\JsonCpp REGULAR_EXPRESSION ${JSONCPP_SOURCES_DIR}/.*)
+endif()
+
