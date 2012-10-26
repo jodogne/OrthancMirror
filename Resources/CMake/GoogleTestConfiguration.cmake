c6a83443b6836a8c91e78a5e662596ff4eef7014
Sebastien Jodogne <s.jodogne@gmail.com>
2012-10-26 14:05:29 +0200

    gtest on debian sid

--- Resources/CMake/GoogleTestConfiguration.cmake
+++ Resources/CMake/GoogleTestConfiguration.cmake
@@ -1,5 +1,11 @@
-if (DEBIAN_USE_STATIC_GOOGLE_TEST)
-  message(FATAL_ERROR "todo")
+if (DEBIAN_USE_GTEST_SOURCE_PACKAGE)
+  set(GTEST_SOURCES /usr/src/gtest/src/gtest-all.cc)
+  include_directories(/usr/src/gtest)
+
+  if (NOT EXISTS /usr/include/gtest/gtest.h OR
+      NOT EXISTS ${GTEST_SOURCES})
+    message(FATAL_ERROR "Please install the libgtest-dev package")
+  endif()
 
 elseif (STATIC_BUILD OR NOT USE_DYNAMIC_GOOGLE_TEST)
   SET(GTEST_SOURCES_DIR ${CMAKE_BINARY_DIR}/gtest-1.6.0)
@@ -16,6 +22,10 @@ elseif (STATIC_BUILD OR NOT USE_DYNAMIC_GOOGLE_TEST)
 
 else()
   include(FindGTest)
+  if (NOT GTEST_FOUND)
+    message(FATAL_ERROR "Unable to find GoogleTest")
+  endif()
+
   include_directories(${GTEST_INCLUDE_DIRS})
   link_libraries(${GTEST_LIBRARIES})
 endif()
