678a09168de3440de2b333fe36740ac153ea21ec
jodogne <none@none>
2013-01-23 13:29:47 +0100

    credentials in python rest toolbox

--- Resources/Samples/Python/RestToolbox.py
+++ Resources/Samples/Python/RestToolbox.py
@@ -2,12 +2,24 @@ import httplib2
 import json
 from urllib import urlencode
 
+_credentials = None
+
+def SetCredentials(username, password):
+    global _credentials
+    _credentials = (username, password)
+
+def _SetupCredentials(h):
+    global _credentials
+    if _credentials != None:
+        h.add_credentials(_credentials[0], _credentials[1])
+
 def DoGet(uri, data = {}):
     d = ''
     if len(data.keys()) > 0:
         d = '?' + urlencode(data)
 
     h = httplib2.Http()
+    _SetupCredentials(h)
     resp, content = h.request(uri + d, 'GET')
     if not (resp.status in [ 200 ]):
         raise Exception(resp.status)
@@ -20,6 +32,7 @@ def DoGet(uri, data = {}):
 
 def _DoPutOrPost(uri, method, data, contentType):
     h = httplib2.Http()
+    _SetupCredentials(h)
 
     if isinstance(data, str):
         body = data
@@ -45,7 +58,9 @@ def _DoPutOrPost(uri, method, data, contentType):
 
 def DoDelete(uri):
     h = httplib2.Http()
+    _SetupCredentials(h)
     resp, content = h.request(uri, 'DELETE')
+
     if not (resp.status in [ 200 ]):
         raise Exception(resp.status)
     else:
