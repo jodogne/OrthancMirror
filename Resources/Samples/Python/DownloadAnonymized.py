bb1c35181fa6e0f8583ff253da0e31132fec7ef4
Sebastien Jodogne <s.jodogne@gmail.com>
2013-01-28 16:58:07 +0100

    samples of anonymization

--- Resources/Samples/Python/DownloadAnonymized.py
+++ Resources/Samples/Python/DownloadAnonymized.py
@@ -5,10 +5,12 @@
 URL = 'http://localhost:8042'
 
 #
-# This sample code will anonymize all the patients that are stored in
-# Orthanc.
+# This sample code will download a ZIP file for each patient that has
+# been anonymized in Orthanc.
 #
 
+import os
+import os.path
 import sys
 import RestToolbox
 
@@ -19,9 +21,11 @@ for patient in RestToolbox.DoGet('%s/patients' % URL):
     # the result of a previous anonymization
     infos = RestToolbox.DoGet('%s/patients/%s' % (URL, patient))
     name = infos['MainDicomTags']['PatientName'].lower()
-    if not name.startswith('anonymized'):
+    if name.startswith('anonymized'):
 
-        # Trigger the anonymization
-        RestToolbox.DoPost('%s/patients/%s/anonymize' % (URL, patient),
-                           { 'Keep' : [ 'SeriesDescription',
-                                        'StudyDescription' ] })
+        # Trigger the download
+        print 'Downloading %s' % name
+        zipContent = RestToolbox.DoGet('%s/patients/%s/archive' % (URL, patient))
+        f = open(os.path.join('/tmp', name + '.zip'), 'wb')
+        f.write(zipContent)
+        f.close()
