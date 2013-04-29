2db623d59386d54ac1ff325ad2791e6931a762fa
Sebastien Jodogne <s.jodogne@gmail.com>
2013-04-29 12:36:39 +0200

    dynamically linking against Mongoose

--- Resources/CMake/MongooseConfiguration.cmake
+++ Resources/CMake/MongooseConfiguration.cmake
@@ -1,33 +1,48 @@
-SET(MONGOOSE_SOURCES_DIR ${CMAKE_BINARY_DIR}/mongoose)
-DownloadPackage("http://mongoose.googlecode.com/files/mongoose-3.1.tgz" "${MONGOOSE_SOURCES_DIR}" "" "")
+if (STATIC_BUILD OR NOT USE_DYNAMIC_MONGOOSE)
+  SET(MONGOOSE_SOURCES_DIR ${CMAKE_BINARY_DIR}/mongoose)
+  DownloadPackage("http://mongoose.googlecode.com/files/mongoose-3.1.tgz" "${MONGOOSE_SOURCES_DIR}" "" "")
 
-# Patch mongoose
-execute_process(
-  COMMAND patch mongoose.c ${CMAKE_SOURCE_DIR}/Resources/Patches/mongoose-patch.diff
-  WORKING_DIRECTORY ${MONGOOSE_SOURCES_DIR}
-  )
-
-include_directories(
-  ${MONGOOSE_SOURCES_DIR}
-  )
-
-list(APPEND THIRD_PARTY_SOURCES
-  ${MONGOOSE_SOURCES_DIR}/mongoose.c
-  )
-
-
-if (${ENABLE_SSL})
-  add_definitions(
-    -DNO_SSL_DL=1
+  # Patch mongoose
+  execute_process(
+    COMMAND patch mongoose.c ${CMAKE_SOURCE_DIR}/Resources/Patches/mongoose-patch.diff
+    WORKING_DIRECTORY ${MONGOOSE_SOURCES_DIR}
     )
-  if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-    link_libraries(dl)
+
+  include_directories(
+    ${MONGOOSE_SOURCES_DIR}
+    )
+
+  list(APPEND THIRD_PARTY_SOURCES
+    ${MONGOOSE_SOURCES_DIR}/mongoose.c
+    )
+
+
+  if (${ENABLE_SSL})
+    add_definitions(
+      -DNO_SSL_DL=1
+      )
+    if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+      link_libraries(dl)
+    endif()
+
+  else()
+    add_definitions(
+      -DNO_SSL=1   # Remove SSL support from mongoose
+      )
   endif()
 
-else()
-  add_definitions(
-    -DNO_SSL=1   # Remove SSL support from mongoose
-    )
-endif()
+  source_group(ThirdParty\\Mongoose REGULAR_EXPRESSION ${MONGOOSE_SOURCES_DIR}/.*)
 
-source_group(ThirdParty\\Mongoose REGULAR_EXPRESSION ${MONGOOSE_SOURCES_DIR}/.*)
+else()
+  CHECK_INCLUDE_FILE_CXX(mongoose.h HAVE_MONGOOSE_H)
+  if (NOT HAVE_MONGOOSE_H)
+    message(FATAL_ERROR "Please install the mongoose-devel package")
+  endif()
+
+  CHECK_LIBRARY_EXISTS(mongoose "mg_start" HAVE_MONGOOSE_LIB)
+  if (NOT HAVE_MONGOOSE_LIB)
+    message(FATAL_ERROR "Please install the mongoose-devel package")
+  endif()
+
+  link_libraries(mongoose)
+endif()
