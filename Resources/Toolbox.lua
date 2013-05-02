bd5473748ae456c0f4fbbc1a67e9eb2a7cdc2545
Sebastien Jodogne <s.jodogne@gmail.com>
2013-05-02 16:34:00 +0200

    lua filter of new instances

--- Resources/Toolbox.lua
+++ Resources/Toolbox.lua
@@ -1,18 +1,18 @@
---[[ printRecursive(struct, [limit], [indent])   Recursively print arbitrary data. 
+--[[ PrintRecursive(struct, [limit], [indent])   Recursively print arbitrary data. 
 Set limit (default 100) to stanch infinite loops.
 Indents tables as [KEY] VALUE, nested tables as [KEY] [KEY]...[KEY] VALUE
 Set indent ("") to prefix each line:    Mytable [KEY] [KEY]...[KEY] VALUE
 Source: https://gist.github.com/stuby/5445834#file-rprint-lua
 --]]
 
-function printRecursive(s, l, i) -- recursive Print (structure, limit, indent)
+function PrintRecursive(s, l, i) -- recursive Print (structure, limit, indent)
    l = (l) or 100; i = i or "";	-- default item limit, indent string
    if (l<1) then print "ERROR: Item limit reached."; return l-1 end;
    local ts = type(s);
    if (ts ~= "table") then print (i,ts,s); return l-1 end
    print (i,ts);           -- print "table"
    for k,v in pairs(s) do  -- print "[KEY] VALUE"
-      l = printRecursive(v, l, i.."\t["..tostring(k).."]");
+      l = PrintRecursive(v, l, i.."\t["..tostring(k).."]");
       if (l < 0) then break end
    end
    return l
--- UnitTests/Lua.cpp
+++ UnitTests/Lua.cpp
@@ -5,64 +5,68 @@
 
 TEST(Lua, Simple)
 {
-  try
+  Orthanc::LuaContext lua;
+  lua.Execute(Orthanc::EmbeddedResources::LUA_TOOLBOX);
+  lua.Execute("a={}");
+  lua.Execute("a['x'] = 10");
+  lua.Execute("a['y'] = {}");
+  lua.Execute("a['y'][1] = 20");
+  lua.Execute("a['y'][2] = 20");
+  lua.Execute("PrintRecursive(a)");
+
+  lua.Execute("function f(a) print(a.bool) return a.bool,20,30,40,50,60 end");
+
+  Json::Value v, vv, o;
+  //v["a"] = "b";
+  v.append("hello");
+  v.append("world");
+  v.append("42");
+  vv.append("sub");
+  vv.append("set");
+  v.append(vv);
+  o = Json::objectValue;
+  o["x"] = 10;
+  o["y"] = 20;
+  o["z"] = 20.5f;
+  v.append(o);
+
   {
-    Orthanc::LuaContext lua;
-    lua.Execute(Orthanc::EmbeddedResources::LUA_TOOLBOX);
-    lua.Execute("a={}");
-    lua.Execute("a['x'] = 10");
-    lua.Execute("a['y'] = {}");
-    lua.Execute("a['y'][1] = 20");
-    lua.Execute("a['y'][2] = 20");
-    lua.Execute("rPrint(a)");
-
-    lua.Execute("function f(a) print(a.bool) return a.bool,20,30,40,50,60 end");
-
-    Json::Value v, vv, o;
-    //v["a"] = "b";
-    v.append("hello");
-    v.append("world");
-    v.append("42");
-    vv.append("coucou");
-    vv.append("toi");
-    v.append(vv);
-    o = Json::objectValue;
-    o["x"] = 10;
-    o["y"] = 20;
-    o["z"] = 20.5f;
-    v.append(o);
-
-    {
-      Orthanc::LuaFunctionCall f(lua, "rPrint");
-      f.PushJSON(v);
-      f.Execute();
-    }
-
-    {
-      Orthanc::LuaFunctionCall f(lua, "f");
-      f.PushJSON(o);
-      ASSERT_THROW(f.ExecutePredicate(), Orthanc::LuaException);
-    }
-
-    o["bool"] = false;
-
-    {
-      Orthanc::LuaFunctionCall f(lua, "f");
-      f.PushJSON(o);
-      ASSERT_FALSE(f.ExecutePredicate());
-    }
-
-    o["bool"] = true;
-
-    {
-      Orthanc::LuaFunctionCall f(lua, "f");
-      f.PushJSON(o);
-      ASSERT_TRUE(f.ExecutePredicate());
-    }
-
+    Orthanc::LuaFunctionCall f(lua, "PrintRecursive");
+    f.PushJSON(v);
+    f.Execute();
   }
-  catch (Orthanc::LuaException e)
+
   {
-    std::cerr << "EXCEPTION: [" << e.What() << "]" << std::endl;
+    Orthanc::LuaFunctionCall f(lua, "f");
+    f.PushJSON(o);
+    ASSERT_THROW(f.ExecutePredicate(), Orthanc::LuaException);
   }
+
+  o["bool"] = false;
+
+  {
+    Orthanc::LuaFunctionCall f(lua, "f");
+    f.PushJSON(o);
+    ASSERT_FALSE(f.ExecutePredicate());
+  }
+
+  o["bool"] = true;
+
+  {
+    Orthanc::LuaFunctionCall f(lua, "f");
+    f.PushJSON(o);
+    ASSERT_TRUE(f.ExecutePredicate());
+  }
+}
+
+
+TEST(Lua, Existing)
+{
+  Orthanc::LuaContext lua;
+  lua.Execute("a={}");
+  lua.Execute("function f() end");
+
+  ASSERT_TRUE(lua.IsExistingFunction("f"));
+  ASSERT_FALSE(lua.IsExistingFunction("a"));
+  ASSERT_FALSE(lua.IsExistingFunction("Dummy"));
 }
