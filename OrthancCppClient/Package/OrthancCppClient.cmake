40420f7c42e932ea264f5a1761e772d62b58737a
Sebastien Jodogne <s.jodogne@gmail.com>
2013-07-18 12:36:02 +0200

    compilation of the dll 32/64

--- OrthancCppClient/Package/OrthancCppClient.cmake
+++ OrthancCppClient/Package/OrthancCppClient.cmake
@@ -1,15 +1,8 @@
 # nm -C -D --defined-only libOrthancCppClient.so 
 
-
-cmake_minimum_required(VERSION 2.8)
-
-project(OrthancCppClientTest)
-
-include_directories(${CMAKE_SOURCE_DIR}/Laaw)
+include_directories(${ORTHANC_ROOT}/OrthancCppClient/Package/Laaw)
 
 set(STATIC_BUILD ON)
-set(ORTHANC_ROOT ${CMAKE_SOURCE_DIR}/../..)
-
 include(${ORTHANC_ROOT}/Resources/CMake/DownloadPackage.cmake)
 include(${ORTHANC_ROOT}/Resources/CMake/JsonCppConfiguration.cmake)
 include(${ORTHANC_ROOT}/Resources/CMake/LibCurlConfiguration.cmake)
@@ -26,8 +19,17 @@ elseif (${MSVC})
   add_definitions(-D_CRT_SECURE_NO_WARNINGS=1)  
 endif()
 
+if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+  if (${CMAKE_SIZEOF_VOID_P} EQUAL 4)
+    set(WINDOWS_DEF ${ORTHANC_ROOT}/OrthancCppClient/Package/Build/Windows32.def)
+  elseif (${CMAKE_SIZEOF_VOID_P} EQUAL 8)
+    set(WINDOWS_DEF ${ORTHANC_ROOT}/OrthancCppClient/Package/Build/Windows64.def)
+  else()
+    message(FATAL_ERROR "Support your platform here")
+  endif()
+endif()
+
 add_library(OrthancCppClient SHARED
-  SharedLibrary.cpp
   ${THIRD_PARTY_SOURCES}
   ${ORTHANC_ROOT}/Core/OrthancException.cpp
   ${ORTHANC_ROOT}/Core/Enumerations.cpp
@@ -42,13 +44,15 @@ add_library(OrthancCppClient SHARED
   ${ORTHANC_ROOT}/OrthancCppClient/Study.cpp
   ${ORTHANC_ROOT}/OrthancCppClient/Instance.cpp
   ${ORTHANC_ROOT}/OrthancCppClient/Patient.cpp
+  ${ORTHANC_ROOT}/OrthancCppClient/Package/SharedLibrary.cpp
   ${ORTHANC_ROOT}/Resources/sha1/sha1.cpp
   ${ORTHANC_ROOT}/Resources/md5/md5.c
   ${ORTHANC_ROOT}/Resources/base64/base64.cpp
+  ${WINDOWS_DEF}
   )
 
 if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
-  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--version-script=${CMAKE_SOURCE_DIR}/Laaw/VersionScript.map")
+  set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--version-script=${ORTHANC_ROOT}/OrthancCppClient/Package/Laaw/VersionScript.map")
   target_link_libraries(OrthancCppClient pthread)
 else()
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--allow-multiple-definition -static-libgcc -static-libstdc++")
