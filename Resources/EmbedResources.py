38ccfd15a14100ae13fe96b0064b95e46cd2cc69
Sebastien Jodogne <s.jodogne@gmail.com>
2014-11-03 13:27:27 +0100

    Fix issue #24 (Build fails on OSX when directory has .DS_Store files)

--- Resources/EmbedResources.py
+++ Resources/EmbedResources.py
@@ -86,6 +86,13 @@ while i < len(SOURCES):
         content = {}
         for root, dirs, files in os.walk(pathName):
             base = os.path.relpath(root, pathName)
+
+            # Fix issue #24 (Build fails on OSX when directory has .DS_Store files):
+            # Ignore folders whose name starts with a dot (".")
+            if base.find('/.') != -1:
+                print('Ignoring folder: %s' % root)
+                continue
+
             for f in files:
                 if f.find('~') == -1:  # Ignore Emacs backup files
                     if base == '.':
