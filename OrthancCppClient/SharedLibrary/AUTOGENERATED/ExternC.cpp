e65fcb955f8d93153ee0a26e1b05d7bfe62427fe
Sebastien Jodogne <s.jodogne@gmail.com>
2014-11-04 15:41:29 +0100

    set version

--- OrthancCppClient/SharedLibrary/AUTOGENERATED/ExternC.cpp
+++ OrthancCppClient/SharedLibrary/AUTOGENERATED/ExternC.cpp
@@ -1509,12 +1509,12 @@ this_->LoadTagContent(reinterpret_cast< const char* >(arg0));
 
   LAAW_EXPORT_DLL_API const char* LAAW_CALL_CONVENTION LAAW_EXTERNC_GetFileVersion()
   {
-    return "0.8.0.4";
+    return "0.8.0.5";
   }
 
   LAAW_EXPORT_DLL_API const char* LAAW_CALL_CONVENTION LAAW_EXTERNC_GetFullVersion()
   {
-    return "0.8.4";
+    return "0.8.5";
   }
 
   LAAW_EXPORT_DLL_API void LAAW_CALL_CONVENTION LAAW_EXTERNC_FreeString(char* str)
