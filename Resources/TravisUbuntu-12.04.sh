ef1c44cae12cbbbdcce705e41e0b85ee8bc415ed
Sebastien Jodogne <s.jodogne@gmail.com>
2015-01-08 13:52:46 +0100

    fix

--- Resources/TravisUbuntu-12.04.sh
+++ Resources/TravisUbuntu-12.04.sh
@@ -1,5 +1,4 @@
-#! /bin/sh
-
+#!/bin/sh
 
 set -e
 
@@ -9,6 +8,7 @@ cd Build
 cmake \
     "-DDCMTK_LIBRARIES=CharLS;dcmjpls;wrap;oflog" \
     -DALLOW_DOWNLOADS=ON \
+    -DUSE_SYSTEM_BOOST=OFF \
     -DUSE_SYSTEM_MONGOOSE=OFF \
     -DUSE_SYSTEM_JSONCPP=OFF \
     -DUSE_SYSTEM_GOOGLE_LOG=OFF \
