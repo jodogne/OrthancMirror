3fa7308efe45f1d99a8cc2332bc105329492c8ba
Sebastien Jodogne <s.jodogne@gmail.com>
2015-01-08 13:47:48 +0100

    missing file

---
+++ Resources/TravisUbuntu-12.04.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+
+set -e
+
+mkdir Build
+cd Build
+
+cmake \
+    "-DDCMTK_LIBRARIES=CharLS;dcmjpls;wrap;oflog" \
+    -DALLOW_DOWNLOADS=ON \
+    -DUSE_SYSTEM_MONGOOSE=OFF \
+    -DUSE_SYSTEM_JSONCPP=OFF \
+    -DUSE_SYSTEM_GOOGLE_LOG=OFF \
+    -DUSE_SYSTEM_PUGIXML=OFF \
+    -DUSE_GTEST_DEBIAN_SOURCE_PACKAGE=ON \
+    ..
+
+make
