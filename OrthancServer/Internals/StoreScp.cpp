e807792451b1589e43519d7e45a1df586d21d607
Sebastien Jodogne <s.jodogne@gmail.com>
2015-03-30 15:12:21 +0200

    clang warning removal

--- OrthancServer/Internals/StoreScp.cpp
+++ OrthancServer/Internals/StoreScp.cpp
@@ -182,7 +182,7 @@ namespace Orthanc
 
           // check the image to make sure it is consistent, i.e. that its sopClass and sopInstance correspond
           // to those mentioned in the request. If not, set the status in the response message variable.
-          if ((rsp->DimseStatus == STATUS_Success))
+          if (rsp->DimseStatus == STATUS_Success)
           {
             // which SOP class and SOP instance ?
             if (!DU_findSOPClassAndInstanceInDataSet(*imageDataSet, sopClass, sopInstance, /*opt_correctUIDPadding*/ OFFalse))
