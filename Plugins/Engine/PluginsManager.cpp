6de35f2113b5004f0c180ddc8f962d0d62ab3ff1
jodogne <unknown>
2015-03-31 11:40:29 +0200

    FreeBSD support

--- Plugins/Engine/PluginsManager.cpp
+++ Plugins/Engine/PluginsManager.cpp
@@ -42,7 +42,7 @@
 
 #ifdef WIN32
 #define PLUGIN_EXTENSION ".dll"
-#elif defined(__linux) || defined(__FreeBSD_kernel__)
+#elif defined(__linux) || defined(__FreeBSD_kernel__) || defined(__FreeBSD__)
 #define PLUGIN_EXTENSION ".so"
 #elif defined(__APPLE__) && defined(__MACH__)
 #define PLUGIN_EXTENSION ".dylib"
--- Resources/CMake/BoostConfiguration.cmake
+++ Resources/CMake/BoostConfiguration.cmake
@@ -56,6 +56,7 @@ if (BOOST_STATIC)
 
   if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux" OR
       ${CMAKE_SYSTEM_NAME} STREQUAL "Darwin" OR
+      ${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD" OR
       ${CMAKE_SYSTEM_NAME} STREQUAL "kFreeBSD")
     list(APPEND BOOST_SOURCES
       ${BOOST_SOURCES_DIR}/libs/thread/src/pthread/once.cpp
