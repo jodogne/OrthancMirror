aa422914dc9892815b721e1b9e4798ff3b444946
Sebastien Jodogne <s.jodogne@gmail.com>
2015-09-25 11:02:26 +0200

    hide symbols in plugins

--- Plugins/Samples/Common/OrthancPlugins.cmake
+++ Plugins/Samples/Common/OrthancPlugins.cmake
@@ -6,6 +6,7 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
   link_libraries(uuid)
   SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pthread")
   SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -pthread")
+
 elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
   link_libraries(rpcrt4 ws2_32 secur32)
   if (CMAKE_COMPILER_IS_GNUCXX)
@@ -21,8 +22,16 @@ elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 endif ()
 
 
-if (CMAKE_COMPILER_IS_GNUCXX)
+if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux" OR
+    ${CMAKE_SYSTEM_NAME} STREQUAL "kFreeBSD" OR
+    ${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
   SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--version-script=${SAMPLES_ROOT}/Common/VersionScript.map -Wl,--no-undefined")
+elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+  SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -exported_symbols_list ${CMAKE_SOURCE_DIR}/Plugins/Samples/Common/ExportedSymbols.list")
+endif()
+
+
+if (CMAKE_COMPILER_IS_GNUCXX)
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -pedantic")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic")
 endif()
@@ -33,7 +42,6 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
   # http://sourceware.org/bugzilla/show_bug.cgi?id=10652#c17
   link_libraries(dl rt)
 endif()
-
 
 include_directories(${SAMPLES_ROOT}/../Include/)
 
