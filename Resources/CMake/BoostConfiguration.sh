5b2680da7f6d1b9cf7d484257df61505012546e3
Sebastien Jodogne <s.jodogne@gmail.com>
2016-03-21 15:15:01 +0100

    Upgrade to Boost 1.60.0 for static builds

--- Resources/CMake/BoostConfiguration.sh
+++ Resources/CMake/BoostConfiguration.sh
@@ -14,22 +14,23 @@ set -u
 ##   - Orthanc between 0.6.2 and 0.7.3: Boost 1.54.0
 ##   - Orthanc between 0.7.4 and 0.9.1: Boost 1.55.0
 ##   - Orthanc between 0.9.2 and 0.9.4: Boost 1.58.0
-##   - Orthanc >= 0.9.5: Boost 1.59.0
+##   - Orthanc between 0.9.5 and 1.0.0: Boost 1.59.0
+##   - Orthanc >= 1.0.1: Boost 1.60.0
 
-rm -rf /tmp/boost_1_59_0
-rm -rf /tmp/bcp/boost_1_59_0
+rm -rf /tmp/boost_1_60_0
+rm -rf /tmp/bcp/boost_1_60_0
 
 cd /tmp
-echo "Uncompressing the sources of Boost 1.59.0..."
-tar xfz ./boost_1_59_0.tar.gz 
+echo "Uncompressing the sources of Boost 1.60.0..."
+tar xfz ./boost_1_60_0.tar.gz 
 
 echo "Generating the subset..."
-mkdir -p /tmp/bcp/boost_1_59_0
-bcp --boost=/tmp/boost_1_59_0 thread system locale date_time filesystem math/special_functions algorithm uuid atomic iostreams /tmp/bcp/boost_1_59_0
+mkdir -p /tmp/bcp/boost_1_60_0
+bcp --boost=/tmp/boost_1_60_0 thread system locale date_time filesystem math/special_functions algorithm uuid atomic iostreams program_options /tmp/bcp/boost_1_60_0
 cd /tmp/bcp
 
 echo "Compressing the subset..."
-tar cfz boost_1_59_0_bcpdigest-0.9.5.tar.gz boost_1_59_0
-ls -l boost_1_59_0_bcpdigest-0.9.5.tar.gz
-md5sum boost_1_59_0_bcpdigest-0.9.5.tar.gz
-readlink -f boost_1_59_0_bcpdigest-0.9.5.tar.gz
+tar cfz boost_1_60_0_bcpdigest-1.0.1.tar.gz boost_1_60_0
+ls -l boost_1_60_0_bcpdigest-1.0.1.tar.gz
+md5sum boost_1_60_0_bcpdigest-1.0.1.tar.gz
+readlink -f boost_1_60_0_bcpdigest-1.0.1.tar.gz
