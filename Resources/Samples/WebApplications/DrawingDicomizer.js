53a472ce5d4b42754a3dee712c18cfe46352c387
Leonardo da Silva Campos <leonardoscampos@gmail.com>
2016-12-03 00:44:19 +0000

    fix DrawingDicomizer jquery path

--- Resources/Samples/WebApplications/DrawingDicomizer.js
+++ Resources/Samples/WebApplications/DrawingDicomizer.js
@@ -58,7 +58,7 @@ var server = http.createServer(function(req, response) {
         toolbox.ServeFile('DrawingDicomizer/orthanc.js', response);
       }
       else if (req.url == '/jquery.js') {
-        toolbox.ServeFile('../../../OrthancExplorer/libs/jquery-1.7.2.min.js', response);
+        toolbox.ServeFile('../../../OrthancExplorer/libs/jquery.min.js', response);
       }
       else if (req.url.startsWith('/orthanc')) {
         toolbox.ForwardGetRequest(orthanc, req.url.substr(8), response);
