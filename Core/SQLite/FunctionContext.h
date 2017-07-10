7f1e0635c282f9d7573b2572f2a639a3306cbd58
jodogne <unknown>
2017-07-10 09:28:30 +0200

    fix Debian FTBFS #867789

--- Core/SQLite/FunctionContext.h
+++ Core/SQLite/FunctionContext.h
@@ -37,7 +37,7 @@
 #include "Statement.h"
 
 struct sqlite3_context;
-struct Mem;  // This corresponds to the opaque type "sqlite3_value"
+struct sqlite3_value;
  
 namespace Orthanc
 {
@@ -50,14 +50,14 @@ namespace Orthanc
     private:
       struct sqlite3_context* context_;
       unsigned int argc_;
-      struct ::Mem** argv_;
+      struct sqlite3_value** argv_;
 
       void CheckIndex(unsigned int index) const;
 
     public:
       FunctionContext(struct sqlite3_context* context,
                       int argc,
-                      struct ::Mem** argv);
+                      struct sqlite3_value** argv);
 
       ColumnType GetColumnType(unsigned int index) const;
  
