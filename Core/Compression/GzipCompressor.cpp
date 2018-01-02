e2b707c5b78d63fb4169b385e674f0b3378802c8
Sebastien Jodogne <s.jodogne@gmail.com>
2018-01-02 09:35:07 +0100

    upgrade to year 2018

--- Core/Compression/GzipCompressor.cpp
+++ Core/Compression/GzipCompressor.cpp
@@ -2,7 +2,7 @@
  * Orthanc - A Lightweight, RESTful DICOM Store
  * Copyright (C) 2012-2016 Sebastien Jodogne, Medical Physics
  * Department, University Hospital of Liege, Belgium
- * Copyright (C) 2017 Osimis, Belgium
+ * Copyright (C) 2017-2018 Osimis S.A., Belgium
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
