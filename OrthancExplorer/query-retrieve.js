d482f270ae9e8363a87049508e3e7293d6a624f5
Sebastien Jodogne <s.jodogne@gmail.com>
2018-12-17 10:22:51 +0100

    replaced 'var' by 'let' in Orthanc Explorer's JavaScript

--- OrthancExplorer/query-retrieve.js
+++ OrthancExplorer/query-retrieve.js
@@ -1,13 +1,13 @@
 function JavascriptDateToDicom(date)
 {
-  var s = date.toISOString();
+  let s = date.toISOString();
   return s.substring(0, 4) + s.substring(5, 7) + s.substring(8, 10);
 }
 
 function GenerateDicomDate(days)
 {
-  var today = new Date();
-  var other = new Date(today);
+  let today = new Date();
+  let other = new Date(today);
   other.setDate(today.getDate() + days);
   return JavascriptDateToDicom(other);
 }
@@ -20,11 +20,11 @@ $('#query-retrieve').live('pagebeforeshow', function() {
     async: false,
     cache: false,
     success: function(modalities) {
-      var target = $('#qr-server');
+      let target = $('#qr-server');
       $('option', target).remove();
 
-      for (var i = 0; i < modalities.length; i++) {
-        var option = $('<option>').text(modalities[i]);
+      for (let i = 0; i < modalities.length; i++) {
+        let option = $('<option>').text(modalities[i]);
         target.append(option);
       }
 
@@ -32,7 +32,7 @@ $('#query-retrieve').live('pagebeforeshow', function() {
     }
   });
 
-  var target = $('#qr-date');
+  let target = $('#qr-date');
   $('option', target).remove();
   target.append($('<option>').attr('value', '*').text('Any date'));
   target.append($('<option>').attr('value', GenerateDicomDate(0)).text('Today'));
@@ -46,8 +46,8 @@ $('#query-retrieve').live('pagebeforeshow', function() {
 
 
 $('#qr-echo').live('click', function() {
-  var server = $('#qr-server').val();
-  var message = 'Error: The C-Echo has failed!';
+  let server = $('#qr-server').val();
+  let message = 'Error: The C-Echo has failed!';
 
   $.ajax({
     url: '../modalities/' + server + '/echo',
@@ -75,7 +75,7 @@ $('#qr-echo').live('click', function() {
 
 
 $('#qr-submit').live('click', function() {
-  var query = {
+  let query = {
     'Level' : 'Study',
     'Query' : {
       'AccessionNumber' : '*',
@@ -88,12 +88,12 @@ $('#qr-submit').live('click', function() {
     }
   };
 
-  var field = $('#qr-fields input:checked').val();
+  let field = $('#qr-fields input:checked').val();
   query['Query'][field] = $('#qr-value').val().toUpperCase();
 
-  var modalities = '';
+  let modalities = '';
   $('#qr-modalities input:checked').each(function() {
-    var s = $(this).attr('name');
+    let s = $(this).attr('name');
     if (modalities == '')
       modalities = s;
     else
@@ -105,7 +105,7 @@ $('#qr-submit').live('click', function() {
   }
 
 
-  var server = $('#qr-server').val();
+  let server = $('#qr-server').val();
   $.ajax({
     url: '../modalities/' + server + '/query',
     type: 'POST', 
@@ -130,27 +130,27 @@ $('#qr-submit').live('click', function() {
 
 $('#query-retrieve-2').live('pagebeforeshow', function() {
   if ($.mobile.pageData) {
-    var pageData = DeepCopy($.mobile.pageData);
+    let pageData = DeepCopy($.mobile.pageData);
 
-    var uri = '../queries/' + pageData.uuid + '/answers';
+    let uri = '../queries/' + pageData.uuid + '/answers';
 
     $.ajax({
       url: uri,
       dataType: 'json',
       async: false,
       success: function(answers) {
-        var target = $('#query-retrieve-2 ul');
+        let target = $('#query-retrieve-2 ul');
         $('li', target).remove();
 
-        for (var i = 0; i < answers.length; i++) {
+        for (let i = 0; i < answers.length; i++) {
           $.ajax({
             url: uri + '/' + answers[i] + '/content?simplify',
             dataType: 'json',
             async: false,
             success: function(study) {
-              var series = '#query-retrieve-3?server=' + pageData.server + '&uuid=' + study['StudyInstanceUID'];
+              let series = '#query-retrieve-3?server=' + pageData.server + '&uuid=' + study['StudyInstanceUID'];
 
-              var content = ($('<div>')
+              let content = ($('<div>')
                              .append($('<h3>').text(study['PatientID'] + ' - ' + study['PatientName']))
                              .append($('<p>').text('Accession number: ')
                                      .append($('<b>').text(study['AccessionNumber'])))
@@ -163,10 +163,10 @@ $('#query-retrieve-2').live('pagebeforeshow', function() {
                              .append($('<p>').text('Study date: ')
                                      .append($('<b>').text(FormatDicomDate(study['StudyDate'])))));
 
-              var info = $('<a>').attr('href', series).html(content);
+              let info = $('<a>').attr('href', series).html(content);
               
-              var answerId = answers[i];
-              var retrieve = $('<a>').text('Retrieve all study').click(function() {
+              let answerId = answers[i];
+              let retrieve = $('<a>').text('Retrieve all study').click(function() {
                 ChangePage('query-retrieve-4', {
                   'query' : pageData.uuid,
                   'answer' : answerId,
@@ -188,9 +188,9 @@ $('#query-retrieve-2').live('pagebeforeshow', function() {
 
 $('#query-retrieve-3').live('pagebeforeshow', function() {
   if ($.mobile.pageData) {
-    var pageData = DeepCopy($.mobile.pageData);
+    let pageData = DeepCopy($.mobile.pageData);
 
-    var query = {
+    let query = {
       'Level' : 'Series',
       'Query' : {
         'Modality' : '*',
@@ -211,8 +211,8 @@ $('#query-retrieve-3').live('pagebeforeshow', function() {
         alert('Error during query (C-Find)');
       },
       success: function(answer) {
-        var queryUuid = answer['ID'];
-        var uri = '../queries/' + answer['ID'] + '/answers';
+        let queryUuid = answer['ID'];
+        let uri = '../queries/' + answer['ID'] + '/answers';
 
         $.ajax({
           url: uri,
@@ -220,25 +220,25 @@ $('#query-retrieve-3').live('pagebeforeshow', function() {
           async: false,
           success: function(answers) {
             
-            var target = $('#query-retrieve-3 ul');
+            let target = $('#query-retrieve-3 ul');
             $('li', target).remove();
 
-            for (var i = 0; i < answers.length; i++) {
+            for (let i = 0; i < answers.length; i++) {
               $.ajax({
                 url: uri + '/' + answers[i] + '/content?simplify',
                 dataType: 'json',
                 async: false,
                 success: function(series) {
-                  var content = ($('<div>')
+                  let content = ($('<div>')
                                  .append($('<h3>').text(series['SeriesDescription']))
                                  .append($('<p>').text('Modality: ')
                                          .append($('<b>').text(series['Modality'])))
                                  .append($('<p>').text('ProtocolName: ')
                                          .append($('<b>').text(series['ProtocolName']))));
 
-                  var info = $('<a>').html(content);
+                  let info = $('<a>').html(content);
 
-                  var answerId = answers[i];
+                  let answerId = answers[i];
                   info.click(function() {
                     ChangePage('query-retrieve-4', {
                       'query' : queryUuid,
@@ -265,8 +265,8 @@ $('#query-retrieve-3').live('pagebeforeshow', function() {
 
 $('#query-retrieve-4').live('pagebeforeshow', function() {
   if ($.mobile.pageData) {
-    var pageData = DeepCopy($.mobile.pageData);
-    var uri = '../queries/' + pageData.query + '/answers/' + pageData.answer + '/retrieve';
+    let pageData = DeepCopy($.mobile.pageData);
+    let uri = '../queries/' + pageData.query + '/answers/' + pageData.answer + '/retrieve';
 
     $.ajax({
       url: '../system',
@@ -279,7 +279,7 @@ $('#query-retrieve-4').live('pagebeforeshow', function() {
         $('#retrieve-form').submit(function(event) {
           event.preventDefault();
 
-          var aet = $('#retrieve-target').val();
+          let aet = $('#retrieve-target').val();
           if (aet.length == 0) {
             aet = system['DicomAet'];
           }
