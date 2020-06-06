aaec164df9630767d08f211e7bb5a5fd1e5359a6
Sebastien Jodogne <s.jodogne@gmail.com>
2020-06-06 10:58:36 +0200

    style

--- Core/Logging.cpp
+++ Core/Logging.cpp
@@ -908,8 +908,8 @@ namespace Orthanc
     }
 
     void SetErrorWarnInfoLoggingStreams(std::ostream* errorStream,
-      std::ostream* warningStream,
-      std::ostream* infoStream)
+                                        std::ostream* warningStream,
+                                        std::ostream* infoStream)
     {
       boost::mutex::scoped_lock lock(loggingMutex_);
       std::unique_ptr<LoggingContext> old;
@@ -931,33 +931,27 @@ namespace Orthanc
 
 #ifdef __EMSCRIPTEN__
 
-    FuncStreamBuf<decltype(emscripten_console_error)> 
-      globalEmscriptenErrorStreamBuf(emscripten_console_error);
-    std::unique_ptr<std::ostream> globalEmscriptenErrorStream;
+    static FuncStreamBuf<decltype(emscripten_console_error)> 
+    globalEmscriptenErrorStreamBuf_(emscripten_console_error);
+    static std::unique_ptr<std::ostream> globalEmscriptenErrorStream_;
 
-    FuncStreamBuf<decltype(emscripten_console_warn)>
-      globalEmscriptenWarningStreamBuf(emscripten_console_warn);
-    std::unique_ptr<std::ostream> globalEmscriptenWarningStream;
+    static FuncStreamBuf<decltype(emscripten_console_warn)>
+    globalEmscriptenWarningStreamBuf_(emscripten_console_warn);
+    static std::unique_ptr<std::ostream> globalEmscriptenWarningStream_;
 
-    FuncStreamBuf<decltype(emscripten_console_log)>
-      globalEmscriptenInfoStreamBuf(emscripten_console_log);
-    std::unique_ptr<std::ostream> globalEmscriptenInfoStream;
+    static FuncStreamBuf<decltype(emscripten_console_log)>
+    globalEmscriptenInfoStreamBuf_(emscripten_console_log);
+    static std::unique_ptr<std::ostream> globalEmscriptenInfoStream_;
 
     void EnableEmscriptenLogging()
     {
-      globalEmscriptenErrorStream.reset(new ostream(
-        &globalEmscriptenErrorStreamBuf));
+      globalEmscriptenErrorStream_.reset(new ostream(&globalEmscriptenErrorStreamBuf_));
+      globalEmscriptenWarningStream_.reset(new ostream(&globalEmscriptenWarningStreamBuf_));
+      globalEmscriptenInfoStream_.reset(new ostream(&globalEmscriptenInfoStreamBuf_));
 
-      globalEmscriptenWarningStream.reset(new ostream(
-        &globalEmscriptenWarningStreamBuf));
-
-      globalEmscriptenInfoStream.reset(new ostream(
-        &globalEmscriptenInfoStreamBuf));
-
-      SetErrorWarnInfoLoggingStreams(
-        &globalEmscriptenErrorStream,
-        &globalEmscriptenWarningStream
-        &globalEmscriptenInfoStream);
+      SetErrorWarnInfoLoggingStreams(&globalEmscriptenErrorStream_,
+                                     &globalEmscriptenWarningStream_,
+                                     &globalEmscriptenInfoStream_);
     }
 #endif
   }
