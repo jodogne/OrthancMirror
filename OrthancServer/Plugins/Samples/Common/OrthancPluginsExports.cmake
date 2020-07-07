f05ab3ca2de90382773b2119bd5e732193839090
Sebastien Jodogne <s.jodogne@gmail.com>
2020-07-07 20:08:57 +0200

    adding missing licenses to sample plugins

--- OrthancServer/Plugins/Samples/Common/OrthancPluginsExports.cmake
+++ OrthancServer/Plugins/Samples/Common/OrthancPluginsExports.cmake
@@ -1,3 +1,22 @@
+# Orthanc - A Lightweight, RESTful DICOM Store
+# Copyright (C) 2012-2016 Sebastien Jodogne, Medical Physics
+# Department, University Hospital of Liege, Belgium
+# Copyright (C) 2017-2020 Osimis S.A., Belgium
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 # In Orthanc <= 1.7.1, the instructions below were part of
 # "Compiler.cmake", and were protected by the (now unused) option
 # "ENABLE_PLUGINS_VERSION_SCRIPT" in CMake
