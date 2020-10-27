3491de62817abb6c4964eeb4a39862763294bd71
Sebastien Jodogne <s.jodogne@gmail.com>
2020-10-27 13:29:23 +0100

    upgrade to civetweb 1.13

--- OrthancFramework/Resources/CMake/OrthancFrameworkParameters.cmake
+++ OrthancFramework/Resources/CMake/OrthancFrameworkParameters.cmake
@@ -94,7 +94,9 @@ set(MSVC_MULTIPLE_PROCESSES OFF CACHE BOOL "Add the /MP option to build with mul
 set(EMSCRIPTEN_TARGET_MODE "wasm" CACHE STRING "Sets the target mode for Emscripten (can be \"wasm\" or \"asm.js\")")
 set(EMSCRIPTEN_TRAP_MODE "" CACHE STRING "Sets the trap mode for Emscripten for numeric errors (can notably be empty, or \"clamp\")")
 set(OPENSSL_STATIC_VERSION "1.1.1" CACHE STRING "Version of OpenSSL to be used in static builds (can be \"1.0.2\", or \"1.1.1\")")
+set(CIVETWEB_OPENSSL_API "1.1" CACHE STRING "Version of the OpenSSL API to be used in civetweb in static builds (can be \"1.0\" or \"1.1\"")
 
+mark_as_advanced(CIVETWEB_OPENSSL_API)
 mark_as_advanced(EMSCRIPTEN_TARGET_MODE)
 mark_as_advanced(EMSCRIPTEN_TRAP_MODE)
 mark_as_advanced(SYSTEM_MONGOOSE_USE_CALLBACKS)
