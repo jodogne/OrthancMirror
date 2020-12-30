fba74df84995d022ede9103e3c6cd95a1e4c06f9
Sebastien Jodogne <s.jodogne@gmail.com>
2020-12-30 08:20:26 +0100

    added openapi description

--- OrthancServer/Sources/main.cpp
+++ OrthancServer/Sources/main.cpp
@@ -1771,6 +1771,13 @@ int main(int argc, char* argv[])
 
         openapi["info"]["version"] = ORTHANC_VERSION;
         openapi["info"]["title"] = "Orthanc API";
+        openapi["info"]["description"] =
+          "This is the full documentation of the [REST API](https://book.orthanc-server.com/users/rest.html) "
+          "of Orthanc.<p>This reference is automatically generated from the source code of Orthanc. A "
+          "[shorter cheat sheet](https://book.orthanc-server.com/users/rest-cheatsheet.html) is part of "
+          "the Orthanc Book.<p>An earlier, manually crafted version from August 2019, is [still available]"
+          "(2019-08-orthanc-openapi.html), but is not up-to-date anymore ([source]"
+          "(https://groups.google.com/g/orthanc-users/c/NUiJTEICSl8/m/xKeqMrbqAAAJ)).";
 
         Json::Value server = Json::objectValue;
         server["url"] = "https://demo.orthanc-server.com/";
