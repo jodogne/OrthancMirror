904ea49f8cb7b9fe006c8256a3b69a97c6cc5547
Alain Mazy <am@osimis.io>
2021-12-02 15:11:32 +0100

    clarified error message

--- OrthancServer/Plugins/Engine/OrthancPlugins.cpp
+++ OrthancServer/Plugins/Engine/OrthancPlugins.cpp
@@ -82,7 +82,7 @@
 #include <dcmtk/dcmdata/dcdicent.h>
 #include <dcmtk/dcmnet/dimse.h>
 
-#define ERROR_MESSAGE_64BIT "A 64bit version of the Orthanc SDK is necessary and it is currently not available !"
+#define ERROR_MESSAGE_64BIT "A 64bit version of the Orthanc SDK is necessary to use buffers > 4GB and it is currently not available !"
 
 
 namespace Orthanc
