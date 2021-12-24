d485a0a10a2e54c4e1784ed23b3b0ed569edf5e3
Sebastien Jodogne <s.jodogne@gmail.com>
2021-12-24 17:20:05 +0100

    removed openssl exception from main()

--- OrthancServer/Sources/main.cpp
+++ OrthancServer/Sources/main.cpp
@@ -705,7 +705,7 @@ static void PrintVersion(const char* path)
     << "Copyright (C) 2012-2016 Sebastien Jodogne, Medical Physics Department, University Hospital of Liege (Belgium)" << std::endl
     << "Copyright (C) 2017-2021 Osimis S.A. (Belgium)" << std::endl
     << "Copyright (C) 2021-2021 Sebastien Jodogne, ICTEAM UCLouvain (Belgium)" << std::endl
-    << "Licensing GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>, with OpenSSL exception." << std::endl
+    << "Licensing GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>." << std::endl
     << "This is free software: you are free to change and redistribute it." << std::endl
     << "There is NO WARRANTY, to the extent permitted by law." << std::endl
     << std::endl
