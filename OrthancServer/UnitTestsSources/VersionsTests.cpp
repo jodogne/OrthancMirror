79ecbb17b633d97a3c5f21ca21fa8bf42b52fd4f
Sebastien Jodogne <s.jodogne@gmail.com>
2022-08-15 14:06:25 +0200

    upgrade to Boost 1.80.0

--- OrthancServer/UnitTestsSources/VersionsTests.cpp
+++ OrthancServer/UnitTestsSources/VersionsTests.cpp
@@ -112,7 +112,7 @@ TEST(Versions, ZlibStatic)
 
 TEST(Versions, BoostStatic)
 {
-  ASSERT_STREQ("1_69", BOOST_LIB_VERSION);
+  ASSERT_STREQ("1_80", BOOST_LIB_VERSION);
 }
 
 TEST(Versions, CurlStatic)
