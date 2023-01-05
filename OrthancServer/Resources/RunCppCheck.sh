c01e2d73bad507915cec17a31041369a83c64338
Alain Mazy <am@osimis.io>
2023-01-05 18:03:25 +0100

    cppcheck

--- OrthancServer/Resources/RunCppCheck.sh
+++ OrthancServer/Resources/RunCppCheck.sh
@@ -26,7 +26,7 @@ syntaxError:../../OrthancFramework/UnitTestsSources/ZipTests.cpp:132
 syntaxError:../../OrthancServer/UnitTestsSources/UnitTestsMain.cpp:310
 uninitMemberVar:../../OrthancServer/Sources/ServerJobs/StorageCommitmentScpJob.cpp:416
 unreadVariable:../../OrthancFramework/Sources/FileStorage/StorageAccessor.cpp
-unreadVariable:../../OrthancServer/Sources/OrthancRestApi/OrthancRestModalities.cpp:1115
+unreadVariable:../../OrthancServer/Sources/OrthancRestApi/OrthancRestModalities.cpp:1118
 unusedFunction
 useInitializationList:../../OrthancFramework/Sources/Images/PngReader.cpp:90
 useInitializationList:../../OrthancFramework/Sources/Images/PngWriter.cpp:98
