7b40d985a27620dc228d42d201b7d830ac109c44
Alain Mazy <am@osimis.io>
2023-02-03 14:32:53 +0100

    Orthanc-1.11.3

--- OrthancFramework/Resources/CMake/OrthancFrameworkParameters.cmake
+++ OrthancFramework/Resources/CMake/OrthancFrameworkParameters.cmake
@@ -24,7 +24,7 @@
 #####################################################################
 
 # Version of the build, should always be "mainline" except in release branches
-set(ORTHANC_VERSION "mainline")
+set(ORTHANC_VERSION "1.11.3")
 
 # Version of the database schema. History:
 #   * Orthanc 0.1.0 -> Orthanc 0.3.0 = no versioning
@@ -38,7 +38,7 @@ set(ORTHANC_DATABASE_VERSION 6)
 # Version of the Orthanc API, can be retrieved from "/system" URI in
 # order to check whether new URI endpoints are available even if using
 # the mainline version of Orthanc
-set(ORTHANC_API_VERSION "18")
+set(ORTHANC_API_VERSION "19")
 
 
 #####################################################################
