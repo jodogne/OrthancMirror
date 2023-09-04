53fcb3af4de0f409c5444afa27bd427414dbf830
Sebastien Jodogne <s.jodogne@gmail.com>
2023-09-04 07:44:52 +0200

    simplifying new signature of Toolbox::ComputeMD5()

--- OrthancFramework/Sources/Toolbox.cpp
+++ OrthancFramework/Sources/Toolbox.cpp
@@ -500,28 +500,14 @@ namespace Orthanc
   void Toolbox::ComputeMD5(std::string& result,
                            const std::set<std::string>& data)
   {
-    md5_state_s state;
-    md5_init(&state);
+    std::string s;
 
-    if (data.size() > 0)
+    for (std::set<std::string>::const_iterator it = data.begin(); it != data.end(); ++it)
     {
-      for (std::set<std::string>::const_iterator it = data.begin(); it != data.end(); ++it)
-      {
-        md5_append(&state, 
-                   reinterpret_cast<const md5_byte_t*>(it->c_str()), 
-                   static_cast<int>(it->size()));
-      }
+      s += *it;
     }
 
-    md5_byte_t actualHash[16];
-    md5_finish(&state, actualHash);
-
-    result.resize(32);
-    for (unsigned int i = 0; i < 16; i++)
-    {
-      result[2 * i] = GetHexadecimalCharacter(static_cast<uint8_t>(actualHash[i] / 16));
-      result[2 * i + 1] = GetHexadecimalCharacter(static_cast<uint8_t>(actualHash[i] % 16));
-    }
+    ComputeMD5(result, s);
   }
 
 #endif
