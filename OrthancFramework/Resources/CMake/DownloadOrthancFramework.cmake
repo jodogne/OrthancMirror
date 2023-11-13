edff4d6113e450de4d586b148f005c0ef7d7b884
Sebastien Jodogne <s.jodogne@gmail.com>
2023-11-13 18:13:45 +0100

    new location of mercurial server

--- OrthancFramework/Resources/CMake/DownloadOrthancFramework.cmake
+++ OrthancFramework/Resources/CMake/DownloadOrthancFramework.cmake
@@ -271,7 +271,7 @@ if (ORTHANC_FRAMEWORK_SOURCE STREQUAL "hg")
   else()
     message("Forking the Orthanc source repository using Mercurial")
     execute_process(
-      COMMAND ${ORTHANC_FRAMEWORK_HG} clone "https://hg.orthanc-server.com/orthanc/"
+      COMMAND ${ORTHANC_FRAMEWORK_HG} clone "https://orthanc.uclouvain.be/hg/orthanc/"
       WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
       RESULT_VARIABLE Failure
       )    
