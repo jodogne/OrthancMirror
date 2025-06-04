b21a0289475acbf1427b26a58f1cce8e3f09f348
Alain Mazy <am@orthanc.team>
2025-06-04 10:38:20 +0200

    rename

--- OrthancServer/Sources/ServerEnumerations.h
+++ OrthancServer/Sources/ServerEnumerations.h
@@ -210,7 +210,7 @@ namespace Orthanc
     MetadataType_MainDicomSequences = 16,        // New in Orthanc 1.11.1
     MetadataType_Instance_PixelDataVR = 17,      // New in Orthanc 1.12.1
     MetadataType_Patient_IsProtected = 18,       // New in Orthanc 1.12.99  (used only by DB plugins)
-    MetadataType_Patient_UnprotectDate = 19,     // New in Orthanc 1.12.99  (used only by DB plugins)
+    MetadataType_Patient_PatientRecyclingOrder = 19,  // New in Orthanc 1.12.99  (used only by DB plugins)
     
     // Make sure that the value "65535" can be stored into this enumeration
     MetadataType_StartUser = 1024,
