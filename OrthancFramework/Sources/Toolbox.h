444a4edf849966502ef3772eb57b457ba8a3aebe
Alain Mazy <am@orthanc.team>
2025-07-22 12:31:54 +0200

    Toolbox::MergeJson

--- OrthancFramework/Sources/Toolbox.h
+++ OrthancFramework/Sources/Toolbox.h
@@ -403,6 +403,8 @@ namespace Orthanc
     static void WriteStyledJson(std::string& target,
                                 const Json::Value& source);
 
+    static void MergeJson(Json::Value &a, const Json::Value &b);
+
     static void RemoveSurroundingQuotes(std::string& value);
 
     class ORTHANC_PUBLIC ElapsedTimer : public boost::noncopyable
