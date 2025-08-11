67346816d210ef6727be6b265400350c0db68175
Sebastien Jodogne <s.jodogne@gmail.com>
2025-08-11 13:39:40 +0200

    moving the code model generation from orthanc-java to orthanc

---
+++ OrthancServer/Resources/CodeModel/docker-internal.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Orthanc - A Lightweight, RESTful DICOM Store
+# Copyright (C) 2012-2016 Sebastien Jodogne, Medical Physics
+# Department, University Hospital of Liege, Belgium
+# Copyright (C) 2017-2023 Osimis S.A., Belgium
+# Copyright (C) 2024-2025 Orthanc Team SRL, Belgium
+# Copyright (C) 2021-2025 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+set -ex
+
+python3 /source/OrthancServer/Resources/CodeModel/GenerateCodeModel.py \
+        --libclang=libclang-14.so.1 \
+        --source /source/OrthancServer/Plugins/Include/orthanc/OrthancCPlugin.h \
+        --target /target/OrthancPluginCodeModel.json
