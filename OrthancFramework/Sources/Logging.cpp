5d32605a5efad97b691c6713f9d05041d991e355
Sebastien Jodogne <s.jodogne@gmail.com>
2025-10-29 22:37:47 +0100

    pthread_setname_np() is not available on linux standard base

--- OrthancFramework/Sources/Logging.cpp
+++ OrthancFramework/Sources/Logging.cpp
@@ -31,8 +31,8 @@
 #include <stdint.h>
 
 #if defined(__linux__) && !defined(NDEBUG)
-  #include <pthread.h>
-#endif              
+#  include <pthread.h>
+#endif
 
 
 /*********************************************************
@@ -663,7 +663,7 @@ namespace Orthanc
 
       threadNames_[id] = name;
 
-#if defined(__linux__) && !defined(NDEBUG)
+#if defined(__linux__) && !defined(NDEBUG) && !defined(__LSB_VERSION__)
       // set the thread name at "system" level too -> required to have the thread names visible in GDB !
       pthread_setname_np(pthread_self(), name.substr(0, 15).c_str());  // thread names are limited to 15 in Linux
 #endif              
