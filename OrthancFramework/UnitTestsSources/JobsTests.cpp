de9cd424310307e6971da9fbc6fb8645f983e921
Alain Mazy <am@orthanc.team>
2025-11-10 18:44:19 +0100

    cppcheck: invalidLifetime -> re-enabled JobsEngine.SequenceOfOperationsJob

--- OrthancFramework/UnitTestsSources/JobsTests.cpp
+++ OrthancFramework/UnitTestsSources/JobsTests.cpp
@@ -733,25 +733,26 @@ TEST(JobsEngine, SubmitAndWait)
 }
 
 
-TEST(JobsEngine, DISABLED_SequenceOfOperationsJob)
+TEST(JobsEngine, SequenceOfOperationsJob)
 {
   JobsEngine engine(10);
   engine.SetThreadSleep(10);
   engine.SetWorkersCount(3);
   engine.Start();
 
-  SequenceOfOperationsJob* job = NULL;
-
   {
     std::string id;
-    std::unique_ptr<SequenceOfOperationsJob> a(new SequenceOfOperationsJob);
-    job = a.get();
-    engine.GetRegistry().Submit(id, a.release(), 0);
+    std::unique_ptr<SequenceOfOperationsJob> seq(new SequenceOfOperationsJob);
+    engine.GetRegistry().Submit(id, seq.release(), 0);
   }
 
   boost::this_thread::sleep(boost::posix_time::milliseconds(500));
 
   {
+    SequenceOfOperationsJob* job = NULL;
+    std::unique_ptr<SequenceOfOperationsJob> seq(new SequenceOfOperationsJob);
+    job = seq.get();
+
     SequenceOfOperationsJob::Lock lock(*job);
     size_t i = lock.AddOperation(new LogJobOperation);
     size_t j = lock.AddOperation(new LogJobOperation);
