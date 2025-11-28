f9e973faa267c33554ebdc5901da12d894a19b52
Alain Mazy <am@orthanc.team>
2025-11-28 17:55:40 +0100

    reverted AppleClang specific flags: moved to postgresql since it seems it is the only projet that requires it so far

--- OrthancFramework/Resources/CMake/Compiler.cmake
+++ OrthancFramework/Resources/CMake/Compiler.cmake
@@ -251,10 +251,6 @@ elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
   # it seems that some recent MacOS compilers don't set these flags correctly which prevents zlib from building correctly
   SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_LARGEFILE64_SOURCE=1 -D_FILE_OFFSET_BITS=64")
 
-  if (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
-    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DSTRERROR_R_INT=1 -D_POSIX_C_SOURCE=200112L")
-  endif()
-
   add_definitions(
     -D_XOPEN_SOURCE=1
     )
