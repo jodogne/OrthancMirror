50c1a1d35ce7a15e8fe9565cf44848792e4cc097
Sebastien Jodogne <s.jodogne@gmail.com>
2025-12-09 11:49:01 +0100

    autogeneration of DicomConformanceStatement.txt

--- OrthancServer/Resources/DicomConformanceStatement.py
+++ OrthancServer/Resources/DicomConformanceStatement.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # Orthanc - A Lightweight, RESTful DICOM Store
 # Copyright (C) 2012-2016 Sebastien Jodogne, Medical Physics
@@ -26,29 +26,84 @@
 # This file injects the UID information into the DICOM conformance
 # statement of Orthanc
 
+import io
+import json
+import os
+import pystache
 import re
+import requests
+import tarfile
 
-# Read the conformance statement of Orthanc
-with open('DicomConformanceStatement.txt', 'r') as f:
-    statements = f.readlines()
+BASE = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-# Create an index of all the DICOM UIDs that are known to DCMTK
-uids = {}
-with open('/usr/include/dcmtk/dcmdata/dcuid.h', 'r') as dcmtk:
-    for l in dcmtk.readlines():
-        m = re.match(r'#define UID_(.+?)\s*"(.+?)"', l)
-        if m != None:
-            uids[m.group(1)] = m.group(2)
 
-# Loop over the lines of the statement, looking for the "|" separator
-with open('/tmp/DicomConformanceStatement.txt', 'w') as f:
-    for l in statements:
-        m = re.match(r'(\s*)(.*?)(\s*)\|.*$', l)
-        if m != None:
-            name = m.group(2)
-            uid = uids[name]
-            f.write('%s%s%s| %s\n' % (m.group(1), name, m.group(3), uid))
+# Read the current version of DCMTK
+with open(os.path.join(BASE, 'OrthancFramework', 'Resources', 'CMake', 'OrthancFrameworkParameters.cmake'), 'r') as f:
+    r = re.search(r'set\(DCMTK_STATIC_VERSION "([0-9.]+)" CACHE STRING', f.read())
+    assert(r != None)
+    version = r.group(1)
+    url = 'https://orthanc.uclouvain.be/downloads/third-party-downloads/dcmtk-%s.tar.gz' % version
+    r = requests.get(url)
+    r.raise_for_status()
 
-        else:
-            # No "|" in this line, just output it
-            f.write(l)
+    dcuid = None
+    tar = tarfile.open(fileobj = io.BytesIO(r.content), mode = 'r:gz')
+    for i in tar.getmembers():
+        if i.name.endswith('dcmdata/include/dcmtk/dcmdata/dcuid.h'):
+            dcuid = tar.extractfile(i)
+
+    assert(dcuid != None)
+    dcmtk = dcuid.readlines()
+
+
+# Create an index of all the DICOM Store SCP UIDs that are known to DCMTK
+all_store_scp = []
+longest = 0
+for l in dcmtk:
+    m = re.match(r'#define UID_(.+?Storage)\s*"(.+?)"', l.decode('ascii'))
+    if m != None:
+        longest = max(longest, len(m.group(1)))
+        all_store_scp.append({
+            'name' : m.group(1),
+            'uid' : m.group(2),
+        })
+
+all_store_scp = sorted(all_store_scp, key = lambda x: x['name'])
+
+for i in range(len(all_store_scp)):
+    all_store_scp[i]['name'] = all_store_scp[i]['name'].ljust(longest)
+
+store_scp = list(filter(lambda x: not x['name'].startswith('DRAFT') and not x['name'].startswith('RETIRED'), all_store_scp))
+retired_store_scp = list(filter(lambda x: x['name'].startswith('RETIRED'), all_store_scp))
+draft_store_scp = list(filter(lambda x: x['name'].startswith('DRAFT'), all_store_scp))
+
+
+# Read all the transfer syntaxes
+with open(os.path.join(BASE, 'OrthancFramework', 'Resources', 'CodeGeneration', 'DicomTransferSyntaxes.json')) as f:
+    a = json.loads(f.read())
+
+transfer_syntaxes = []
+longest = 0
+for i in range(len(a)):
+    item = {
+        'name' : '%sTransferSyntax' % a[i]['Value'],
+        'uid' : a[i]['UID'],
+    }
+    transfer_syntaxes.append(item)
+    longest = max(longest, len(item['name']))
+
+transfer_syntaxes = sorted(transfer_syntaxes, key = lambda x: x['uid'])
+
+for i in range(len(transfer_syntaxes)):
+    transfer_syntaxes[i]['name'] = transfer_syntaxes[i]['name'].ljust(longest)
+
+
+# Inject into the template conformance statement of Orthanc
+with open(os.path.join(os.path.dirname(__file__), 'DicomConformanceStatement.mustache'), 'r') as f:
+    with open(os.path.join(os.path.dirname(__file__), 'DicomConformanceStatement.txt'), 'w') as g:
+        g.write(pystache.render(f.read(), {
+            'store_scp' : store_scp,
+            'draft_store_scp' : draft_store_scp,
+            'retired_store_scp' : retired_store_scp,
+            'transfer_syntaxes' : transfer_syntaxes,
+        }))
