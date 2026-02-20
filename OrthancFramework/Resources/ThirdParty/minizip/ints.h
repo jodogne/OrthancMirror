a55ab1a8783a3b02210d2f357be8c7be1c53ec1d
Sebastien Jodogne <s.jodogne@gmail.com>
2026-02-20 16:47:30 +0100

    replaced ints.h from minizip by stdint.h

--- OrthancFramework/Resources/ThirdParty/minizip/ints.h
+++ OrthancFramework/Resources/ThirdParty/minizip/ints.h
@@ -1,57 +1,16 @@
-/* ints.h -- create integer types for 8, 16, 32, and 64 bits
- * Copyright (C) 2024 Mark Adler
- * For conditions of distribution and use, see the copyright notice in zlib.h
- *
- * There exist compilers with limits.h, but not stdint.h or inttypes.h.
- */
-
 #ifndef INTS_H
 #define INTS_H
-#include <limits.h>
-#if defined(UCHAR_MAX) && UCHAR_MAX == 0xff
-    typedef signed char i8_t;
-    typedef unsigned char ui8_t;
-#else
-#   error "no 8-bit integer"
-#endif
-#if defined(USHRT_MAX) && USHRT_MAX == 0xffff
-    typedef short i16_t;
-    typedef unsigned short ui16_t;
-#elif defined(UINT_MAX) && UINT_MAX == 0xffff
-    typedef int i16_t;
-    typedef unsigned ui16_t;
-#else
-#   error "no 16-bit integer"
-#endif
-#if defined(UINT_MAX) && UINT_MAX == 0xffffffff
-    typedef int i32_t;
-    typedef unsigned ui32_t;
-#   define PI32 "d"
-#   define PUI32 "u"
-#elif defined(ULONG_MAX) && ULONG_MAX == 0xffffffff
-    typedef long i32_t;
-    typedef unsigned long ui32_t;
-#   define PI32 "ld"
-#   define PUI32 "lu"
-#else
-#   error "no 32-bit integer"
-#endif
-#if defined(ULONG_MAX) && ULONG_MAX == 0xffffffffffffffff
-    typedef long i64_t;
-    typedef unsigned long ui64_t;
-#   define PI64 "ld"
-#   define PUI64 "lu"
-#elif defined(ULLONG_MAX) && ULLONG_MAX == 0xffffffffffffffff
-    typedef long long i64_t;
-    typedef unsigned long long ui64_t;
-#   define PI64 "lld"
-#   define PUI64 "llu"
-#elif defined(ULONG_LONG_MAX) && ULONG_LONG_MAX == 0xffffffffffffffff
-    typedef long long i64_t;
-    typedef unsigned long long ui64_t;
-#   define PI64 "lld"
-#   define PUI64 "llu"
-#else
-#   error "no 64-bit integer"
-#endif
+
+#include <stdint.h>   // This one is provided by Orthanc
+
+typedef int16_t   i16_t;
+typedef int32_t   i32_t;
+typedef int64_t   i64_t;
+typedef int8_t    i8_t;
+
+typedef uint16_t  ui16_t;
+typedef uint32_t  ui32_t;
+typedef uint64_t  ui64_t;
+typedef uint8_t   ui8_t;
+
 #endif
