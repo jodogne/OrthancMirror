aa573d97b5ce7c0d4547be846de55635b37fc33a
Sebastien Jodogne <s.jodogne@gmail.com>
2026-08-12 17:17:49 +0200

    fix build on older version of cmake

--- OrthancFramework/Resources/CMake/DcmtkConfigurationStatic-3.7.0.cmake
+++ OrthancFramework/Resources/CMake/DcmtkConfigurationStatic-3.7.0.cmake
@@ -52,6 +52,8 @@ endif()
 DownloadPackage(${DCMTK_MD5} ${DCMTK_URL} "${DCMTK_SOURCES_DIR}")
 
 
+list(APPEND ORTHANC_PATCHES_LIST "dcmtk-3.7.0-max-nested-sequence.patch")  # Must be out of "FirstRun"
+
 if (FirstRun)
   # Apply the patches
   execute_process(
@@ -65,7 +67,6 @@ if (FirstRun)
     message(FATAL_ERROR "Error while patching a file")
   endif()
 
-  list(APPEND ORTHANC_PATCHES_LIST "dcmtk-3.7.0-max-nested-sequence.patch")
   execute_process(
     COMMAND ${PATCH_EXECUTABLE} -p0 -N -i
     ${CMAKE_CURRENT_LIST_DIR}/../Patches/dcmtk-3.7.0-max-nested-sequence.patch
